f2b301bd4e9374c187da8939b6edcc7f718a2488
Daniel Odey <dannyandcode@gmail.com>
2023-11-12 00:45:22 +0100

    high order array methods challenges

--- index.js
+++ index.js
@@ -214,37 +214,195 @@ const sixDigits = arrNames.filter((word) => {
 console.log(sixDigits);
 
 
-const companies = [
-    {name: 'company one', category: 'finance', start: 1981, end: 2004},
-    {name: 'company two', category: 'retail', start: 1992, end: 2008},
-    {name: 'company three', category: 'auto', start: 1999, end: 2007},
-    {name: 'company four', category: 'retail', start: 1989, end: 2010},
-    {name: 'company five', category: 'technology', start: 2009, end: 2014},
-    {name: 'company six', category: 'finance', start: 198, end: 2010},
-    {name: 'company seven', category: 'auto', start: 1986, end: 2016},
-    {name: 'company eight', category: 'technology', start: 2011, end: 2016},
-    {name: 'company nine', category: 'retail', start: 1981, end: 1989},
+// const companies = [
+//     {name: 'company one', category: 'finance', start: 1981, end: 2004},
+//     {name: 'company two', category: 'retail', start: 1992, end: 2008},
+//     {name: 'company three', category: 'auto', start: 1999, end: 2007},
+//     {name: 'company four', category: 'retail', start: 1989, end: 2010},
+//     {name: 'company five', category: 'technology', start: 2009, end: 2014},
+//     {name: 'company six', category: 'finance', start: 198, end: 2010},
+//     {name: 'company seven', category: 'auto', start: 1986, end: 2016},
+//     {name: 'company eight', category: 'technology', start: 2011, end: 2016},
+//     {name: 'company nine', category: 'retail', start: 1981, end: 1989},
     
-];
+// ];
 
 //get only retail companies
 
-const retailCompanies = companies.filter((ret) => {
-    return ret.category === 'retail';
-});
+// const retailCompanies = companies.filter((ret) => {
+//     return ret.category === 'retail';
+// });
 
-console.log(retailCompanies);
+// console.log(retailCompanies);
 
 //get companies that started in or after 1980 and ended in or before 2005
 
-const dateCompanies = companies.filter((company) => {
-    return company.start >= 1980 && company.end <= 2005;
-})
+// const dateCompanies = companies.filter((company) => {
+//     return company.start >= 1980 && company.end <= 2005;
+// })
 
-console.log(dateCompanies);
+// console.log(dateCompanies);
 
 //filter returns anything that is true on the right side of the array.
 
 //array.map
 
 //maps can return anything you need it to
+
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const multNum = numArr.map((number) => number * 2);
+
+console.log(multNum);
+
+const companies = [
+    {name: 'company one', category: 'finance', start: 1981, end: 2004},
+    {name: 'company two', category: 'retail', start: 1992, end: 2008},
+    {name: 'company three', category: 'auto', start: 1999, end: 2007},
+    {name: 'company four', category: 'retail', start: 1989, end: 2010},
+    {name: 'company five', category: 'technology', start: 2009, end: 2014},
+    {name: 'company six', category: 'finance', start: 1981, end: 2010},
+    {name: 'company seven', category: 'auto', start: 1986, end: 2016},
+    {name: 'company eight', category: 'technology', start: 2011, end: 2016},
+    {name: 'company nine', category: 'retail', start: 1981, end: 1989},
+    
+];
+
+//create an array with company names
+
+const companyNames = companies.map((names) => names.name);
+
+console.log(companyNames);
+
+//create an array with just company and category
+
+const companyDetail = companies.map((companyNames) => {
+    return {
+        name: companyNames.name,
+        category: companyNames.category,
+    }
+});
+
+console.log(companyDetail);
+
+//create an array of objects with the name oof the company and the length of each company in year
+
+
+const companyAge = companies.map((company) => {
+    return {
+        name: company.name,
+        age: company.end - company.start,
+    }
+});
+
+console.log(companyAge);
+
+//chain map methods
+//you can chain the map method to have multiple .maps on one function. you can also chain multiple methods.
+
+//reduce
+
+//accumulator and previous value mean the same thing
+
+const reNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const addAll = reNum.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+
+}, 0);
+
+console.log(addAll);
+
+const cart = [
+    {id: 1, name: 'product 1', price: 130},
+    {id: 2, name: 'product 2', price: 150},
+    {id: 3, name: 'product 3', price: 175},
+];
+//use reduce to get the total of the prices
+
+const priceTotal = cart.reduce(function (acc, product) {
+    return acc + product.price;
+}, 0);
+
+console.log(priceTotal);
+
+//array method challenges
+
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john@gmail.com',
+    phone: '111-111-1111',
+    age: 30,
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Poe',
+    email: 'jane@gmail.com',
+    phone: '222-222-2222',
+    age: 25,
+  },
+  {
+    firstName: 'Bob',
+    lastName: 'Foe',
+    email: 'bob@gmail.com',
+    phone: '333-333-3333',
+    age: 45,
+  },
+  {
+    firstName: 'Sara',
+    lastName: 'Soe',
+    email: 'Sara@gmail.com',
+    phone: '444-444-4444',
+    age: 19,
+  },
+  {
+    firstName: 'Jose',
+    lastName: 'Koe',
+    email: 'jose@gmail.com',
+    phone: '555-555-5555',
+    age: 23,
+  },
+];
+
+// const youngPeople = people.map((name) => {
+//     return {
+//         firstName: name.firstName,
+//         lastName: name.lastName,
+//         email: name.email,
+
+//     }
+
+// });
+
+const youngPeople = people.filter((x) => x.age <= 25)
+    .map((z) => {
+        return {
+            firstName: z.firstName,
+            lastName: z.lastName,
+            email: z.email
+        }
+    });
+
+
+
+console.log(youngPeople);
+
+//add all of the possitive numbers in the array
+
+const numberss = [2, -30, 50, 20, -12, -9, 7];
+
+const addition = numberss.filter((con) => con > 0)
+     .reduce((acc, curr) => acc + curr, 0);
+
+console.log(addition);
+
+
+const words = ['coder', 'programmer', 'developer'];
+
+//create an array called capitalizedWords with the words from the `words` array with the first letter of each word capitalized.
+
+const capitalizedWords = words.map((word) => word.slice(0,1).toUpperCase() + word.slice(1));
+console.log(capitalizedWords);
+
